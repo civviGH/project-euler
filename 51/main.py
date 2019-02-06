53a689057102b72bd369cee1b4b20ce678bf514b
CV <voss@ins.uni-bonn.de>
2019-02-06 16:40:13 +0100

    Do things better, faster, stronger

---
+++ 51/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from aux import *
+
+primes = generatePrimes(10, 100000)
+
+def generateList(n, s):
+    1
+
+def numberOfPrimes(l):
+    number = 0
+    for p in l:
+        if checkPrime(p):
+            number += 1
+    return number
+
+for p in primes:
+    for i in range(1, len(str(p))):
