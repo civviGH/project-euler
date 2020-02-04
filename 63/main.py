ebc278781fc253ca983ebc1cdea1a354c2f38fe9
Christopher Voss <voss@ins.uni-bonn.de>
2020-02-04 12:14:03 +0100

    output von 63 angepasst

--- 63/main.py
+++ 63/main.py
@@ -4,6 +4,8 @@ from aux import *
 
 base = 1
 hits = 0
+baselist = [0] * 9
+print(baselist)
 while base < 10:
     print(f"Starting with base: {base}")
     s = 1
@@ -15,8 +17,11 @@ while base < 10:
         s = len(str(num))
         if (s == exponent):
             hits += 1
+            baselist[base-1] += 1
             # print(f"Hit: {base}^{exponent} = {num}\t{hits}")
     print(f"Stopped because {s} < {exponent}")
     base += 1
 
-print(hits)
+for i in range(9):
+    print(f"{i+1}\t{baselist[i]}")
+# print(hits)
